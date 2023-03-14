fdb6097981391034a6b24553dabea2c0ffdd4cb1
sathwikchalla <sathwikchalla123@gmail.com>
2023-03-14 16:10:45 +0530

    First Commit

---
+++ DemoApp/src/in/challa/main/DemoApp.java
@@ -0,0 +1,17 @@
+package in.challa.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] a= {10,20,30};
+		for(int ele : a) {
+			System.out.println(ele);
+		}
+		String name="sachin";
+		int count=name.toUpperCase().length();
+		System.out.println("the length of the string is:" +count);
+
+	}
+
+}
